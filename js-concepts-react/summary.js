4cfc2176fa515e7f7b04ea87c7fedd18b3f8b7c3
hasanisu <hasanjimam@gmail.com>
2022-10-31 18:27:50 +0900

    summary

---
+++ js-concepts-react/summary.js
@@ -0,0 +1,55 @@
+/*
+1. variable (let, const) three types of variable
+1.2 condition (<,> ===, !==, <=, >=) && ||, if-else
+1.3 array: declare, length, index, push, pop, indexof, includes
+1.4 for loop, while
+1.5 function return parameter
+1.6 object property, including array, object
+
+---------------------
+ES&
+1. template sting ${}
+2. spread(...)copy an array then add a new element to an array 
+2.1 use filter to remove an element from an array 
+3. arrow function 
+3.1 no parameter 
+3.2 sigle parameter 
+3.3 multiple parameter 
+3.4 multi line
+4. destructuring : object destructuring array destructuring
+5. object shorthand 
+6. fuction parameter default value 
+7. optional chainig (?.)
+
+----------------------------
+Browse API
+1. local storage session storage
+2. location API
+3. History API
+4. fetch
+
+-----------------------------
+Others
+1. array : map, forEach, filter, find
+2. ternary operator
+3. logical and logical or && ||
+4. JSON (stringify, parse)
+*/
+
+const x = 50;
+const Y = 100;
+const obj ={
+    x: x,
+    y: Y,
+};
+
+// shortcut
+const obj1 = {x, y}
+
+const name = 'solimullah';
+const area = 'dhaka';
+const nobab ={
+    name: name,
+    area: area
+};
+const nobab2 = {name, area};
